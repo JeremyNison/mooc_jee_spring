4909afcc915162f6ee8d4d468ca9062c455bfebc
Nison Jeremy <jeremy.nison.etu@b10p14.fil.univ-lille.fr>
2022-09-07 17:25:52 +0200

    Exo 2 fini

--- week1/exo102/WEB-INF/classes/AuthServlet.java
+++ week1/exo102/WEB-INF/classes/AuthServlet.java
@@ -4,41 +4,45 @@ import java.io.Writer;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
 
 
 @WebServlet("/auth")
 public class AuthServlet extends HttpServlet {
 
-	// TODO : only handle POST request for authentication
-	// @Override
-	protected void doXXX(HttpServletRequest req, HttpServletResponse resp) 
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
 	throws ServletException, IOException {
-		// TODO : get login / password from request parameters
-		String login = null;
-		String password = null;
+		String login = req.getParameter("login");
+		String password = req.getParameter("password");
 		
 		if ( login == null || password == null ) throw new ServletException("no login/password");
-		boolean succeed = "admin".equals(login) && "admin".equals(password);
+		boolean succeed = "admin@foo.com".equals(login) && "admin".equals(password);
 		
-		// TODO : if auth is OK, 
-		  // add something in session for next calls, 
-		  // then redirect to "welcome.jsp"
-
-		// TODO : if auth KO
-		  // set an "errorMessage" in request attribute
-		  // forward to auth.jsp with request dispatcher
-
+		if(succeed){ 
+			req.getSession().setAttribute("logged", (boolean) true);
+			//req.getRequestDispatcher("welcome.jsp").forward(req,resp);
+			resp.sendRedirect("welcome.jsp");
+		} else{
+			req.setAttribute("errorMessage", "Error : Wrong username or password");
+			req.getRequestDispatcher("auth.jsp").forward(req,resp);
+		}
 	}
 	
-	// TODO : allow to disconnect with a GET to /auth with any parameter "logout" value
-	// @Override
-	protected void doYYY(HttpServletRequest req, HttpServletResponse resp) 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 	throws ServletException, IOException {
-	  // TODO : check for "logout" parameter
-	  //   if so : disconnect and show auth.jsp
-	  //   if not : Error 500
+		String logout = (String) req.getParameter("logout");
+
+		if(logout != null){
+			req.getSession().setAttribute("logged", (boolean) false);
+			req.setAttribute("logoutMessage", "Sucessfully logged out.");
+			req.getRequestDispatcher("auth.jsp").forward(req,resp);
+		}
+		resp.sendError(500);
 	}
 
 }
--- week1/exo102/welcome.jsp
+++ week1/exo102/welcome.jsp
@@ -1,6 +1,12 @@
 <%
 // TODO : check if authenticated !
 //   if not : show an error an a link to auth.jsp
+
+Boolean isLogged = (Boolean) session.getAttribute("logged");
+if(isLogged == null || !isLogged){
+    out.print("<div>Please <a href=\"auth.jsp\">log in</a></div>");
+    return;
+}
 %>
 <h1>Welcome Admin !</h1>
 
